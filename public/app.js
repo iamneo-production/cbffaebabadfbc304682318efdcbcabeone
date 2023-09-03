926317a798bb4f71ebbb0d0a127804fffde546bb
Iamneo <techadmin@iamneo.ai>
2023-09-03 18:30:15 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -62,3 +62,97 @@ btns.forEach((btn, i) => {
 });
 
 document.querySelector('#reset').addEventListener('click', resetGame);
+
+var x = true;
+var o = false;
+
+function myFunction(btnid) {
+var button = document.getElementById(btnid);
+
+    if (x) {
+        button.value = "X";
+        button.disabled = true; // Disable the button after setting the value
+        conditionCheck("X");
+        //  updateParagraph("X");
+        x = false;
+        o = true;
+    } else {
+        button.value = "O";
+        button.disabled = true; // Disable the button after setting the value
+        conditionCheck("O");
+        //  updateParagraph("O");
+        x = true;
+        o = false;
+    }
+}
+
+function conditionCheck(val) {
+    var r00 = document.getElementById("r00");
+    var r01 = document.getElementById("r01");
+    var r02 = document.getElementById("r02");
+    var r10 = document.getElementById("r10");
+    var r11 = document.getElementById("r11");
+    var r12 = document.getElementById("r12");
+    var r20 = document.getElementById("r20");
+    var r21 = document.getElementById("r21");
+    var r22 = document.getElementById("r22");
+    var buttons = [
+        [r00, r01, r02],
+        [r10, r11, r12],
+        [r20, r21, r22],
+        [r00, r10, r20],
+        [r01, r11, r21],
+        [r02, r12, r22],
+        [r00, r11, r22],
+        [r02, r11, r20]
+    ];
+
+    for (var i = 0; i < buttons.length; i++) {
+        var [btn1, btn2, btn3] = buttons[i];
+        if (btn1.value === btn2.value && btn2.value === btn3.value) {
+            if (btn1.value === "X") {
+                updateParagraph("N"); // Player X wins
+            } else if (btn1.value === "O") {
+                updateParagraph("M"); // Player O wins
+            }
+            return; // Exit the loop if we have a winner
+        }
+        else{
+            updateParagraph(val); 
+        }
+    }
+
+    // Check for a draw
+    var draw = true;
+    for (var i = 0; i < buttons.length; i++) {
+        for (var j = 0; j < buttons[i].length; j++) {
+            if (buttons[i][j].value === "") {
+                draw = false;
+                break;
+            }
+        }
+    }
+
+    if (draw) {
+        updateParagraph("D"); // It's a draw
+    }
+}
+
+
+function updateParagraph(element) {
+    var el = element;
+    var paragraph = document.getElementById("p1");
+    var reset = document.getElementById("reset");
+
+    if (el === "X") {
+        paragraph.textContent = "Player O's turn";
+    } else if (el === "O") {
+        paragraph.textContent = "Player X's turn";
+    } else if (el === "N") {
+        paragraph.textContent = "Player X won";
+        reset.disabled = false;
+    } else if (el === "M") {
+        paragraph.textContent = "Player O won";
+        reset.disabled = false;
+    }
+}
